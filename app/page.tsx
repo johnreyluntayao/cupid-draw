68160e3153a0f9ef5741d330c70eda8d61e00ce3
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2026-02-06 20:46:58 +0800

    add margins

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+
+import Hero from "@/components/ui/Hero";
+
 export default function Home() {
   return (
     <div className="min-h-screen mx-sm md:mx-md lg:mx-lg">
-      
+      <Hero/>
     </div>
   );
 }
