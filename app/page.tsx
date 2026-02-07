6c04a2d7a7f1ca1b618bf9baa26dbeef2174d6dd
John Cloyd Refani <jhnrefani@gmail.com>
2026-02-07 17:56:08 +0800

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Footer from "@/components/atoms/Footer";
 import Header from "@/components/atoms/Header";
 
 export default function Home() {
   return (
     <div className="min-h-screen mx-sm md:mx-md lg:mx-lg">
-      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
